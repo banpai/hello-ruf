44768984d1e222f4d7474bce3a3fcd7159aea951
banpai <banpai@yeah.net>
2017-09-01 23:15:48 +0800

    添加学习fetch 控制led等，通过网页控制

--- src/index.js
+++ src/index.js
@@ -1,24 +1,63 @@
 'use strict';
 
+var fetch = require('fetch');
+
+
+
+function postState(state) {
+    // fetch("http://banpai.vicp.io/ruff-code.html").then(function (response) {
+    //     if (response.status !== 200) {
+    //         console.log("存在一个问题，状态码为：" + response.status);
+    //         return;
+    //     }
+    //     //检查响应文本
+    //     response.json().then(function (data) {
+    //         console.log(data);
+    //     });
+    // }).catch(function (err) {
+    //     console.log("Fetch错误:" + err);
+    // })
+
+    fetch('http://banpai.vicp.io/ruff-code.html')
+    .then(function (response) {
+        return response.text();
+    })
+    .then(function (json) {
+        console.log(json.substring(10, 11));
+        if(json.substring(10, 11) == '1'){
+            $('#led-r').turnOn();
+        }else{
+            $('#led-r').turnOff();
+        }
+    });
+}
+
 $.ready(function (error) {
     if (error) {
         console.log(error);
         return;
     }
     console.log('l2232');
-    // $('#led-r').turnOn();
 
-    $('#button').on('push', function(){
-        console.log('按钮按下。');
-        $('#led-r').turnOn();
-    });
+    // setInterval(function(){
+    //     postState();
+    // }, 1000);
 
-    $('#button').on('release', function(){
-        console.log('松开按钮。');
-        $('#led-r').turnOff();
-    })
+    // $('#button').on('push', function () {
+    //     console.log('按钮按下。');
+    //     $('#led-r').turnOn(function () {
+    //         postState('turn on');
+    //     });
+    // });
 
-    $('#led-r').turnOn();
+    // $('#button').on('release', function () {
+    //     console.log('松开按钮。');
+    //     $('#led-r').turnOff(function () {
+    //         postState('turn off');
+    //     });
+    // })
+
+     $('#led-r').turnOn();
 });
 
 $.end(function () {
